720b018190e2cbe108d5c35984726313dae5e77d
eo-eo-eo <eduardosipov13@outlook.com>
2025-05-25 23:06:58 +0800

    Update Scripthelper.lua

--- Scripthelper.lua
+++ Scripthelper.lua
@@ -1,12 +1,4 @@
-local owner = "Upbolt"
-local branch = "revision"
-
-local function webImport(file)
-    return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/Hydroxide/%s/%s.lua"):format(owner, branch, file)), file .. '.lua')()
-end
-
-webImport("init")
-webImport("ui/main")
+loadstring(game:HttpGet("https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/dex.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/eo-eo-eo/extras/refs/heads/main/Remote.Bind%20fire%20tool%20open%20source.lua"))()
