59ba764fc19ef78273c7880f0b3e5d3b585c6ea6
eo-eo-eo <eduardosipov13@outlook.com>
2025-08-13 02:23:01 +0100

    Update Scripthelper.lua

--- Scripthelper.lua
+++ Scripthelper.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpyBeta.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/InfernusScripts/Octo-Spy/refs/heads/main/Main.lua", true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/dex.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/eo-eo-eo/extras/refs/heads/main/Remote.Bind%20fire%20tool%20open%20source.lua"))()
